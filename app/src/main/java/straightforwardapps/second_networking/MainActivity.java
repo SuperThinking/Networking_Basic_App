1efd58c198d1c80429f3e15d65c197b2413545f3
Vishal Dhawan <superdhawan@gmail.com>
2017-12-10 17:43:17 +0530

    Initial commit

---
+++ app/src/main/java/straightforwardapps/second_networking/MainActivity.java
@@ -0,0 +1,100 @@
+package straightforwardapps.second_networking;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText rno;
+    TextView mac, st;
+    String mac_add = "AC:C3:3A:DE:F0:B6";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rno = (EditText) findViewById(R.id.rno);
+        mac = (TextView) findViewById(R.id.mac);
+        st = (TextView) findViewById(R.id.st);
+
+        mac.setText(mac_add);
+
+    }
+
+    public void sendshiz(View view) {
+        new SendPostRequest().execute();
+    }
+
+    public class SendPostRequest extends AsyncTask<String, String, String> {
+
+        String us = rno.getText().toString();
+        String pas = mac.getText().toString();
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try
+            {
+                URL url = new URL("https://android-club-project.herokuapp.com/upload_details?reg_no="+us+"&"+"mac="+pas);
+                HttpURLConnection x = (HttpURLConnection) url.openConnection();
+                x.connect();
+
+                InputStream stream = x.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                String line = "";
+                StringBuffer buffer = new StringBuffer( );
+                while((line=reader.readLine())!=null)
+                {
+                    buffer.append(line);
+                }
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return "IDK";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            st.setText(s);
+        }
+    }
+}
